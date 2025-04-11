23e501b33e2f5937b85bd5c8e2d153488b5dd2ca
ZoDD-Script <blessedchukwu89@gmail.com>
2025-04-11 12:48:58 +0100

    final setup for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDB } from "./config/db.js";
-dotenv.config();
 
 import ProductRouter from "./routes/product.routes.js";
 
-const app = express();
-app.use(express.json());
+dotenv.config();
 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Server is Ready");
-});
+const __dirname = path.resolve();
 
+app.use(express.json());
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use("/api/products", ProductRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  0;
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
